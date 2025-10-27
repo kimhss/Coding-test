f6f98a98a1d74bbcaba9e0c81e717fc96ce5069c
kimhss <127471807+kimhss@users.noreply.github.com>
2025-10-27 14:13:19 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(n != 0){
+            if ((n % 10) % 3 == 0 && n % 10 != 0) {
+                answer++;
+            }
+            n /= 10;
+        }
+        return answer;
+    }
+}
