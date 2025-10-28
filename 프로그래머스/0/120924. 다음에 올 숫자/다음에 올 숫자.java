5177b68c63bc811d015687d9a62b3184cc5131d8
kimhss <127471807+kimhss@users.noreply.github.com>
2025-10-28 21:45:27 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        if (common[1] - common[0] == common[2] - common[1]) {
+            // 등차수열
+            answer = common[common.length - 1] + common[1] - common[0];
+        }
+        else {
+            int num = common[1] / common[0];
+            answer = common[common.length - 1] * num;
+        }
+        return answer;
+    }
+}
