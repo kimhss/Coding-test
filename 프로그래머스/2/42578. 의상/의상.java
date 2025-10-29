126705d30cbf28b8db0fe4fdc8b214b5d71abec2
kimhss <127471807+kimhss@users.noreply.github.com>
2025-10-29 15:34:50 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        for(String key : map.keySet()) {
+            answer *= (map.get(key) + 1); // 조합 -> 안입는 경우도 고려하기 위해 + 1
+        }
+        
+        answer -= 1; // 모두 '안입음'일 경우 -1 해주기 
+        return answer;
+    }
+}
