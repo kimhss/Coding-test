4f68358376d69d99ddd47121aaad8d9e8fb9e018
kimhss <127471807+kimhss@users.noreply.github.com>
2025-11-13 21:58:44 +0900

    [level 0] Title: 모음 제거, Time: 0.13 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,9 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        
-        answer = my_string.replaceAll("a|e|i|o|u", "");
-        
+        answer = my_string.replaceAll("[aeiou]", "");
         return answer;
     }
 }
