c97ae81d431338c69babda44d59695fb4dec923d
kimhss <127471807+kimhss@users.noreply.github.com>
2026-02-11 21:22:08 +0900

    [Silver I] Title: 미로 탐색, Time: 204 ms, Memory: 18352 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,11 +4,10 @@ public class Main{
     static int N;
     static int M;
     static int[][] arr;
-    static int[][] depth;
-    static boolean[][] visited;
     
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
@@ -16,8 +15,6 @@ public class Main{
         N = Integer.parseInt(s.split(" ")[0]);
         M = Integer.parseInt(s.split(" ")[1]);
         
-        visited = new boolean[N][M];
-        depth = new int[N][M];
         arr = new int[N][M];
         
         for(int i = 0; i < N; i++) {
@@ -31,8 +28,6 @@ public class Main{
         // target x = N - 1; y = M - 1;
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[] {0, 0});
-        visited[0][0] = true;
-        depth[0][0] = 1;
         
         while(!q.isEmpty()) {
             int[] now = q.poll();
@@ -44,16 +39,14 @@ public class Main{
                 int y = now[1] + dy[i];
                 
                 if(x >= N || x < 0 || y >= M || y < 0) continue;
-                if(visited[x][y]) continue;
-                if(arr[x][y] == 0) continue;
+                if(arr[x][y] != 1) continue;
                 
                 q.offer(new int[] {x, y});
-                visited[x][y] = true;
-                depth[x][y] = depth[now[0]][now[1]] + 1;
+                arr[x][y] = arr[now[0]][now[1]] + 1;
             }
         }
         
-        System.out.println(depth[N - 1][M - 1]);
+        System.out.println(arr[N - 1][M - 1]);
         
         sc.close();
         return;
