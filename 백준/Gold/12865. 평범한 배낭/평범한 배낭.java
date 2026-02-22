31c438c1bb9797e1bba4789717981b9a716db993
kimhss <127471807+kimhss@users.noreply.github.com>
2026-02-22 19:20:16 +0900

    [Gold V] Title: 평범한 배낭, Time: 212 ms, Memory: 18364 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,31 +1,30 @@
 import java.util.*;
 import java.io.*;
 public class Main{
-    static int N, K;
-    
+    static int K, N;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
         N = sc.nextInt();
         K = sc.nextInt();
-        int[][] arr = new int[N][2];
-        int[] dp = new int[K+1]; // dp[w] = 무게가 w일때 용량 v
+        
+        int[] dp = new int[K + 1];
         
         for(int i = 0; i < N; i++) {
-            int W = sc.nextInt();  // 무게
-            int V = sc.nextInt();  // 가치
+            int w = sc.nextInt();
+            int v = sc.nextInt();
             
-            arr[i][0] = W;
-            arr[i][1] = V;
-            
-            for (int w = K; w >= W; w--) {
-                dp[w] = Math.max(dp[w], dp[w - W] + V);
+            for(int j = K; j >= w; j--) {
+                dp[j] = Math.max(dp[j], dp[j - w] + v);
             }
         }
         
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < dp.length; i++) {
+            max = Math.max(dp[i], max);
+        }
         
-        System.out.println(dp[K]);
-        
+        System.out.println(max);
         sc.close();
         return;
     }
