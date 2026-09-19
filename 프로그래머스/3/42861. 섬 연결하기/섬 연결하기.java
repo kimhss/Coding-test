adc63b3616d04ffcc5bd99c2e8467f4858155c88
Hyeonsu Kim <127471807+kimhss@users.noreply.github.com>
2026-09-19 16:18:18 +0900

    [level 3] Title: 섬 연결하기, Time: 0.72 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -9,21 +9,19 @@ class Solution {
         
         parent = new int[n];
         
-        // 1. 처음에는 각자 자기 자신이 대표
+        // 처음에는 자기가 자기의 부모
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
         
-        // 2. 다리를 비용순으로 정렬
+        // 크루스칼을 위해 최소 비용순으로 정렬
         Arrays.sort(costs, (a, b) -> a[2] - b[2]);
         
-        // 3. 싼 다리부터 확인
-        for (int[] cost : costs) {
-            int a = cost[0];
-            int b = cost[1];
-            int price = cost[2];
+        for (int i = 0; i < costs.length; i++) {
+            int a = costs[i][0];
+            int b = costs[i][1];
+            int price = costs[i][2];
             
-            // 4. 서로 다른 그룹이면 연결
             if (find(a) != find(b)) {
                 union(a, b);
                 answer += price;
@@ -34,7 +32,6 @@ class Solution {
     }
     
     private static int find(int x) {
-        
         if (parent[x] == x) {
             return x;
         }
