ca64675e22ba7d0d254df5cb2eb03df9190a4244
Hyeonsu Kim <127471807+kimhss@users.noreply.github.com>
2026-09-19 15:12:45 +0900

    [level 3] Title: 등굣길, Time: 0.54 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,50 +1,45 @@
 class Solution {
     static final int MOD = 1000000007;
-
     public int solution(int m, int n, int[][] puddles) {
-
-        int[][] map = new int[n][m];
-        boolean[][] isPuddle = new boolean[n][m];
-
-        // 웅덩이 표시
-        for (int[] puddle : puddles) {
-            int x = puddle[0] - 1;
-            int y = puddle[1] - 1;
-
-            isPuddle[y][x] = true;
+        int answer = 0;
+        
+        int[][] dp = new int[n][m];
+        boolean[][] puddle = new boolean[n][m];
+        
+        for (int i = 0; i < puddles.length; i++) {
+            int x = puddles[i][1] - 1;
+            int y = puddles[i][0] - 1;
+            
+            puddle[x][y] = true;
         }
-
-        // 시작점
-        map[0][0] = 1;
-
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
-                // 시작점은 이미 1로 설정
                 if (i == 0 && j == 0) {
+                    dp[0][0] = 1;
                     continue;
                 }
-
-                // 웅덩이면 갈 수 없음
-                if (isPuddle[i][j]) {
-                    map[i][j] = 0;
+                
+                if (puddle[i][j]) {
+                    dp[i][j] = 0;
                     continue;
                 }
-
-                // 위에서 오는 경우
-                if (i > 0) {
-                    map[i][j] += map[i - 1][j];
-                }
-
-                // 왼쪽에서 오는 경우
+                
+                
                 if (j > 0) {
-                    map[i][j] += map[i][j - 1];
+                    dp[i][j] += dp[i][j - 1];
                 }
-
-                map[i][j] %= MOD;
+                
+                if (i > 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                
+                dp[i][j] %= MOD;
             }
         }
-
-        return map[n - 1][m - 1];
+        
+        answer = dp[n - 1][m - 1];
+        
+        return answer;
     }
 }
